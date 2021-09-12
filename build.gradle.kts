31e8d234e7e3ddb197da55efe37ceb1544ac876c
LOENS2 <leo.suessmeyer@gmail.com>
2021-09-12 13:07:47 +0200

    Added shadowJar support in build.grade.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.30"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 group = "com.loens2"
@@ -12,7 +13,13 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+
+}
+
+tasks.withType<Jar>() {
+    manifest{
+        attributes["Main-Class"] = "com.loens2.autoDynDNS.MainKt"
+    }
 }
 
 tasks.test {
